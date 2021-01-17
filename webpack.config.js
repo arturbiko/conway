3f7e7df6586d3defc7251d1131d36caaeb5f622e
Artur Biko <git@timrhub.com>
2021-01-17 19:54:28 +0100

    Basic build strategy

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,28 @@
 const Encore = require('@symfony/webpack-encore');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
 if (!Encore.isRuntimeEnvironmentConfigured()) {
     Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
 }
 
+const indexOutputConfiguration = Object.assign(
+    {},
+    {template: path.resolve(__dirname + '/index.html')},
+    Encore.isProduction()
+        ? {filename: path.resolve(__dirname + '/public/index.html')}
+        : {}
+);
+
 Encore
     .setOutputPath('public/build/')
-
     .setPublicPath('/build')
 
     .addEntry('app', './client/app.js')
     .addStyleEntry('style', './client/assets/scss/app.scss')
+
+    .addPlugin(new HtmlWebpackPlugin(indexOutputConfiguration))
 
     .splitEntryChunks()
 
@@ -27,6 +39,10 @@ Encore
 
     .enableSassLoader()
     .enableReactPreset()
+
+    .cleanupOutputBeforeBuild()
 ;
 
-module.exports = Encore.getWebpackConfig();
+const config = Encore.getWebpackConfig();
+
+module.exports = config;
